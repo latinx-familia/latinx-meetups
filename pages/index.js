f35ac9fad2140ee150a734f621300d143093621b
Esteban <esteban@atexto.com>
2023-09-27 22:09:25 -0500

    gtm script added and head factorized

--- pages/index.js
+++ pages/index.js
@@ -17,20 +17,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
         <>
-            <Head>
-                <meta charset="UTF-8" />
-                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <meta name="description" content="Book a unique vacation experience with our selection of boats for rent as accommodation in Isla Mujeres, Mexico. Enjoy the beauty of the Caribbean Sea while staying on one of our comfortable and stylish boats. Perfect for a romantic getaway or a fun-filled family vacation. Contact us today to start planning your dream vacation in Isla Mujeres!" />
-                
-                <title>Latinx Meetups</title>
-
-                <meta property="og:url" content="" />
-                <meta property="og:type" content="website" />
-                <meta property="og:title" content="" />
-                <meta property="og:description" content="" />
-                <meta property="og:image" content="" />
-            </Head>
+            <Head />
             <Header />
             <Hero />
             <SectionLeft />
