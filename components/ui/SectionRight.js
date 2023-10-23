504e3932d580fe088c004a6f63bf1dcf5c8b21bf
Esteban Gorupicz <33237895+estebangorupicz@users.noreply.github.com>
2023-10-23 14:36:50 -0500

    Update SectionRight.js

--- components/ui/SectionRight.js
+++ components/ui/SectionRight.js
@@ -16,12 +16,12 @@ export default function SectionRight() {
                     </div>
                     <div className="section-description content" id="toggle-navbar">
                         <div className="button-container">
-                            <Link href="https://app.getriver.io/latinx-tech-group/events" target="_blank">
+                            <Link href="https://forms.gle/1w8h93u1m14iY3YF7" target="_blank">
                                 <button className="secondary-btn hero-btn"><Icon icon="mdi:city" />
                                     SUGGEST A CITY
                                 </button>
                             </Link>
-                            <Link href="https://forms.gle/1w8h93u1m14iY3YF7" target="_blank">
+                            <Link href="https://app.getriver.io/latinx-tech-group/events" target="_blank">
                                 <button className="scroll-btn hero-btn"><Icon icon="mdi:check-outline" />
                                     RSVP FOR NOV 22
                                 </button>
