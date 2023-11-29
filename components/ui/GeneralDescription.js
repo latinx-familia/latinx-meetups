6f47d31113887996523d2b86faacc5a4df837653
Esteban Gorupicz <33237895+estebangorupicz@users.noreply.github.com>
2023-11-29 10:35:54 -0500

    Update GeneralDescription.js

--- components/ui/GeneralDescription.js
+++ components/ui/GeneralDescription.js
@@ -8,7 +8,7 @@ export default function Header () {
                 <h2><p>Latinx Tech Group is a community of VC and Angel investors and Startup Founders all over the world supporting each other to thrive.</p>
                 <p>Founded by Latinx founders in Silicon Valley in 2021, we are currently +1,000 members from 70 cities in 25 countries and want to invite you to join.</p></h2>
                 <div className="arrow-down"></div>
-                <Link href="https://chat.whatsapp.com/BquU18oQxMu9m7hf8gdX5S" target="_blank">
+                <Link href="https://chat.whatsapp.com/INP8jx2vnjVLCFRBzOrOMo" target="_blank">
                     <button className="secondary-btn hero-btn" id="share-boat"><Icon icon="mdi:whatsapp" /> 
                         Join the WhatsApp Community
                     </button>
