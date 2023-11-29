d1709258796591fb1fca485217d471a3286ff093
Esteban Gorupicz <33237895+estebangorupicz@users.noreply.github.com>
2023-11-29 10:16:39 -0500

    Update GeneralDescription.js

--- components/ui/GeneralDescription.js
+++ components/ui/GeneralDescription.js
@@ -5,8 +5,8 @@ export default function Header () {
     return(
         <>
             <section className="general-description" id="stay">
-                <h2><p>Latinx Tech Group is a distributed Whatsapp community of VC and Angel investors and Startup Founders all over the world supporting each other to thrive.</p>
-                <p>Founded by Latinx founders in Silicon Valley in 2021, we are currently +500 members from 35 cities and want to invite you to join.</p></h2>
+                <h2><p>Latinx Tech Group is a community of VC and Angel investors and Startup Founders all over the world supporting each other to thrive.</p>
+                <p>Founded by Latinx founders in Silicon Valley in 2021, we are currently +1,000 members from 70 cities in 25 countries and want to invite you to join.</p></h2>
                 <div className="arrow-down"></div>
                 <Link href="https://chat.whatsapp.com/BquU18oQxMu9m7hf8gdX5S" target="_blank">
                     <button className="secondary-btn hero-btn" id="share-boat"><Icon icon="mdi:whatsapp" /> 
