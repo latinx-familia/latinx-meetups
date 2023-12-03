082a28e66df3eceecde44c8526b4846751170a0a
Esteban Gorupicz <33237895+estebangorupicz@users.noreply.github.com>
2023-12-02 19:51:52 -0500

    Update SectionRight.js

--- components/ui/SectionRight.js
+++ components/ui/SectionRight.js
@@ -17,7 +17,7 @@ export default function SectionRight() {
                     <div className="section-description content" id="toggle-navbar">
                         <div className="button-container">
                             <Link href="https://forms.gle/x1APvVMUg3WhRPX18" target="_blank">
-                                <button className="secondary-btn hero-btn"><Icon icon="mid:city" />
+                                <button className="secondary-btn hero-btn"><Icon icon="mdi:city" />
                                     SUGGEST A CITY
                                 </button>
                             </Link>
