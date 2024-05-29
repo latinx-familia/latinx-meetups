adf9b5911d677681df9252c1543c1c612a9ab1ee
Esteban <esteban@atexto.com>
2024-05-29 06:41:40 -0700

    thank you note on landing

--- components/ui/Hero.js
+++ components/ui/Hero.js
@@ -1,12 +1,13 @@
 import Link from "next/link";
 import { Icon } from '@iconify/react';
+import Image from "next/legacy/image"
 
 export default function Hero () {
     return(
        <>
         <section className="hero">
-                <h1>Let&apos;s come together to connect ❤️</h1>
-                <h2>We&apos;re hosting get-togethers all over the world on May 28, 2024, to celebrate the Latinx Tech Community 🌍🚀💫 </h2>
+                <h1>Gracias por la Magia de Latinx ❤️</h1>
+                <h2>On May 28, 2024, we celebrated the Latinx Tech Community worldwide for the third time. Thank you to you all for making it happen. Let's see you again on August, 27th.</h2>
                 
                 <div className="button-container">
                     <Link href="https://forms.gle/S6GjZFbeLUUGkBJu5" target="_blank">
@@ -14,9 +15,9 @@ export default function Hero () {
                             BE A SPONSOR
                         </button>
                     </Link>
-                    <Link href="https://app.getriver.io/latinx" target="_blank">
+                    <Link href="https://app.getriver.io/latinx?pastEvents=truehttps://www.linkedin.com/search/results/content/?keywords=Latinx&mentionsOrganization=%5B%2288964791%22%5D&origin=GLOBAL_SEARCH_HEADER&sid=*wv&sortBy=%22date_posted%22" target="_blank">
                         <button className="scroll-btn hero-btn" id="goto-rsvp"><Icon icon="mdi:check-outline" />
-                            RSVP FOR MAY 28
+                            MAY 28 MEETUPS
                         </button>
                     </Link>
                 </div>
